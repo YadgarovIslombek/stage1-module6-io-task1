7b9b1996062f674ce1b7b4f9328780e10902b1d5
Islombek <64156252+YadgarovIslombek@users.noreply.github.com>
2023-04-15 20:58:48 +0500

    fixed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+            List<String> profiles = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String row;
+            int i = 0;
+            while ((row=bufferedReader.readLine())!= null){
+                String[] s = row.split(" ");
+//                Profile profile = new Profile(s[0],Integer.parseInt(s[1]),s[2],Long.parseLong(s[3]));
+//                profiles.add(profile);
+
+                profiles.add(i,s[0]);
+                i++;
+            }
+            bufferedReader.close();
+            System.out.println(profiles);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());;
+        }
+
+
+        return new Profile(profiles.get(0),Integer.parseInt(profiles.get(1)),profiles.get(2),Long.parseLong(profiles.get(3)));
     }
 }
