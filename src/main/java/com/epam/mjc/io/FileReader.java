f73a354c5b9938de0dfdfd6ef5e39a0db14d3ce4
Islombek <64156252+YadgarovIslombek@users.noreply.github.com>
2023-04-15 21:08:25 +0500

    fixed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -9,25 +9,26 @@ import java.util.Objects;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-            List<String> profiles = new ArrayList<>();
+        List<String> profiles = new ArrayList<>();
         try {
             BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file));
             String row;
             int i = 0;
-            while ((row = Objects.requireNonNull(bufferedReader).readLine()) != null){
+            while ((row = Objects.requireNonNull(bufferedReader).readLine()) != null) {
                 String[] s = row.split(" ");
 //                Profile profile = new Profile(s[0],Integer.parseInt(s[1]),s[2],Long.parseLong(s[3]));
 //                profiles.add(profile);
 
-                profiles.add(i,s[1]);
+                profiles.add(i, s[1]);
                 i++;
             }
             bufferedReader.close();
         } catch (Exception e) {
-            System.out.println(e.getMessage());;
+            System.out.println(e.getMessage());
+            ;
         }
 
 
-        return new Profile(profiles.get(0),Integer.parseInt(profiles.get(1)),profiles.get(2),Long.parseLong(profiles.get(3)));
+        return new Profile(profiles.get(0), Integer.parseInt(profiles.get(1)), profiles.get(2), Long.parseLong(profiles.get(3)));
     }
 }
